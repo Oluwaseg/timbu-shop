bfff54c7b59574f994cdb72214ef78f828f62b61
Samuel Oluwatobi Oluwasegun <oluwasegunsam56@gmail.com>
2024-07-09 23:33:53 +0100

    Update Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,96 +2,95 @@ import ProductCard from "./ProductCard";
 
 const products = [
   {
-    image: "/public/Retinol.png ",
+    image: "/Retinol.png ",
     name: "Retinol ",
     brand: "Bodycare Lotion",
     price: "18,000",
   },
   {
-    image: "../../public/Bronze.png",
+    image: "/Bronze.png",
     name: "Bronze Goddess",
     brand: "Bodycare Lotion",
     price: "32,430",
   },
   {
-    image: "../../public/La.png",
+    image: "/La.png",
     name: "LA ROCHE-POSAY ",
     brand: "Bodycare Package",
     price: "240,000",
   },
   {
-    image: "../../public/Ka.png",
+    image: "/Ka.png",
     name: "Kalak",
     brand: "Bodycare Package",
     price: "330,200",
   },
   {
-    image: "../../public/PAIXAO.png",
+    image: "/PAIXAO.png",
     name: "PAIXAO",
     brand: "Bodycare Lotion & Body Wash",
     price: "330,200",
   },
   {
-    image: "../../public/NIVEA.png",
+    image: "/NIVEA.png",
     name: "NIVEA",
     brand: "Body Lotion",
     price: "6,000",
   },
   {
-    image: "../../public/VICTORIA.png",
+    image: "/VICTORIA.png",
     name: "VICTORIA'S SECRET",
     brand: "Body Wash",
     price: "42,000",
   },
   {
-    image: "../../public/PANOXYL.png",
+    image: "/PANOXYL.png",
     name: "PanOxyl",
     brand: "Body Wash",
     price: "12,000",
   },
   {
-    image: "../../public/SHEA.png",
+    image: "/SHEA.png",
     name: "Shea",
     brand: "Body Mosturizer & Conditional",
     price: "42,000",
   },
   {
-    image: "../../public/CERAVE.png",
+    image: "/CERAVE.png",
     name: "Cerave",
     brand: "Body Lotion",
     price: "19,200",
   },
   {
-    image: "../../public/NEUTRO.png",
+    image: "/NEUTRO.png",
     name: "NEUTROGENA",
     brand: "Body Oil",
     price: "9,500",
   },
   {
-    image: "../../public/DOVE.png",
+    image: "/DOVE.png",
     name: "Dove",
     brand: "Body Moisturizer",
     price: "32,430",
   },
   {
-    image: "../../public/NIVEA2.png",
+    image: "/NIVEA2.png",
     name: "NIVEA",
     brand: "Bodycare Package",
     price: "41,000",
   },
   {
-    image: "../../public/COCO.png",
+    image: "/COCO.png",
     name: "Cocokind",
     brand: "Body Moisturizer",
     price: "20,200",
   },
   {
-    image: "../../public/SUMMER.png",
+    image: "/SUMMER.png",
     name: "Summer Friday's",
     brand: "Body Lotion",
     price: "210,200",
   },
-  // Add other products here
 ];
 
 const Product = () => {
